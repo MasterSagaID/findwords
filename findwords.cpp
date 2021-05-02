f8615e26a68068319712156e77bcc5915e1d4da4
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2021-05-02 15:57:35 +0700

    Update findwords.cpp
    menambahkan fungsi search vertikal , belum getch vertikal

--- findwords.cpp
+++ findwords.cpp
@@ -21,9 +21,10 @@ const int cols = 16, rows = 15;
                                 "pdcrzmsngrdnrpz",
                                 "ohnkzwaterjgtra"};
 
+char kata_transpor[cols][rows];
 // char *getWordVertical(int);
-// char *reverse(char *words);
-// bool searchVertical(char *words);
+char reverse(char *kata_input);
+bool searchVertical(char *kata_input);
 bool searchHorizontal(char *kata_input);
 
 
@@ -35,7 +36,7 @@ int main()
     cin.get();
     for (int i=0;i<n;i++){
         cin.getline(word,16,'\n');
-        if ( searchHorizontal(word))
+        if ( searchHorizontal(word) || searchHorizontal(word))
             cout << "Ada\n";
         else 
             cout << "Tidak Ada\n";
@@ -52,17 +53,64 @@ bool searchHorizontal(char *kata_input){
             return true;
             break;
             }
-        else if(i == rows-1){
-            return false;
+        else if(i==rows-1){
+                return false;
             }
     }
 return false;
 }
 
-char *getWordVertical(){
-    char Transpor [rows][cols];
 
+bool searchVertical(char *kata_input){
+  
+    for(int i = 0 ; i < rows; i ++){
+        for(int j = 0 ; j<cols ; j++ ){
+            kata_transpor [j][i] = words[i][j];
+        }
+    }
+    for(int i = 0 ; i < rows; i ++){
+        for(int j = 0 ; j< rows ; j++ ){
+            kata_transpor [i][j] ;
+        }
+    }
     
-    retun ptrTrasnpor;
+    for(int i = 0 ; i < rows ; i ++){
+        char *ptrWords[i];
+        if(strstr(*ptrWords,kata_input)){
+            return true;
+                break;
+            }
+            else if(i == rows-1){
+                return false;
+            }
+    }
+return false;
+
 }
+// char *getWordVertical(){
+//     char Transpor_kata [cols][rows];
+
+// }
+
+
+// char reverse(char *kata_input){
+//     for(int i = 0  ; i < rows; i++){
+//         for(int j = cols-1 ; j >=0;j--){
+//             words[i][j];
+//         }
+//     }
+//     char *ptrWords[rows];
+//      for(int i = 0 ; i < rows ; i ++){
+//         *ptrWords = words[i];
+//         if(strstr(*ptrWords,kata_input)){
+//             return true;
+//             break;
+//             }
+//         else if(i == rows-1){
+//             return false;
+//             }
+//     }
+// return false;
+
+// }
 
